aa5291fff05a857e2861475fd26e78c4373faa77
lulopez27 <lulopez@estudiantec.cr>
2020-12-16 21:32:09 -0600

    fix sjeng

--- SPEC/SPEC/458.sjeng/stats/l1d_size/graphics.py
+++ SPEC/SPEC/458.sjeng/stats/l1d_size/graphics.py
@@ -11,7 +11,7 @@ CPI = []
 writebacks = np.zeros(10)
 
 
-for i in range(10):
+for i in range(5):
     rep = 2**(7+i)
     size.append(rep)
     fileo = "SPEC/SPEC/458.sjeng/stats/l1d_size/stats"+str(rep)+".txt"
@@ -33,25 +33,25 @@ for i in range(10):
         elif "system.cpu.committedInsts" in line:
             found = (re.search("([0-9]+)",line))
             insts.append(found[0])
-for i in range(10):
+for i in range(5):
     CPI.append(int(numCycles[i])/int(insts[i]))
 
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 ax1.invert_yaxis()
 ax1.plot(data,'o-')
-ax1.xaxis.set_ticks(np.arange(10))
+ax1.xaxis.set_ticks(np.arange(5))
 ax1.xaxis.set_ticklabels(size)
 ax1.legend(["Misses"])
 ax2.plot(hits,'o-')
 ax2.legend(["Hits"])
-ax2.xaxis.set_ticks(np.arange(10))
+ax2.xaxis.set_ticks(np.arange(5))
 ax2.xaxis.set_ticklabels(size)
 ax3.plot(writebacks,'o-')
 ax3.legend(["WriteBack"])
-ax3.xaxis.set_ticks(np.arange(10))
+ax3.xaxis.set_ticks(np.arange(5))
 ax3.xaxis.set_ticklabels(size)
 ax4.plot(CPI,'o-')
 ax4.legend(["CPI"])
-ax4.xaxis.set_ticks(np.arange(10))
+ax4.xaxis.set_ticks(np.arange(5))
 ax4.xaxis.set_ticklabels(size)
 show()
