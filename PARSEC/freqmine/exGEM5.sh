03537e3dcf6281fb3f3d5a3e2c7aa2b779d1cc13
Skryfall <skryfall@gmail.com>
2020-12-17 19:35:19 -0600

    Stats Kevin

--- PARSEC/freqmine/exGEM5.sh
+++ PARSEC/freqmine/exGEM5.sh
@@ -3,4 +3,4 @@ export OPT=$GEM5_DIR/build/X86/gem5.opt
 export PY=$GEM5_DIR/configs/example/se.py
 export BENCHMARK=./src/freqmine
 export ARGUMENT=./inputs/T10I4D100K_3.dat
-time $OPT -d m5out/ $PY -c $BENCHMARK -o $ARGUMENT -I 100000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=128kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=2 --cacheline_size=16384
+time $OPT -d m5out/ $PY -c $BENCHMARK -o $ARGUMENT -I 100000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=128kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=2 --cacheline_size=32 --bp-type=LocalBP
