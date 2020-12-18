53552d915a8c1b6bc663599ad014ce8bcb636bcd
lulopez27 <lulopez@estudiantec.cr>
2020-12-17 21:18:38 -0600

    Graphycs BP

--- PARSEC/freqmine/stats/BiModeBP/globalPredictorSize/graphics.py
+++ PARSEC/freqmine/stats/BiModeBP/globalPredictorSize/graphics.py
@@ -3,66 +3,62 @@ import sys
 import re
 
 size = []
-missesdcache = []
-missesicache = []
-misses2cache = []
-hitsdcache = []
-hitsicache = []
-hits2cache = []
+BTBHits = []
+BTBLookups = []
+BTBMissPCT = []
+numbranchMispred = []
+numBranches = []
+BranchMispredPct = []
+condPredicted = []
+condIncorrect = []
 
 
-for i in range(5):
-    rep = 2**(5+i)
+for i in range(3):
+    rep = 2**(10+i)
     size.append(rep)
-    fileo = "PARSEC/blackscholes/stats/cacheline/stats"+str(rep)+".txt"
-    with open(fileo) as f:
+    fileo = "PARSEC/freqmine/stats/BiModeBP/globalPredictorSize/stats"+str(rep)+".txt"
+    with open(fileo) as f: #localPredictorSize
         datafile = f.readlines()
     for line in datafile:
-        if "system.cpu.dcache.overall_misses::total" in line:
+        if "system.cpu.branchPred.BTBHits" in line:
             found = (re.search("([0-9]+)",line))
-            missesdcache.append(found[0])
-        elif "system.cpu.dcache.overall_hits::total" in line:
+            BTBHits.append(found[0])
+        elif "system.cpu.branchPred.BTBLookups" in line:
             found = (re.search("([0-9]+)",line))
-            hitsdcache.append(found[0])
-        elif "system.cpu.icache.overall_misses::total" in line:
+            BTBLookups.append(found[0])
+        elif "system.cpu.Branches" in line:
             found = (re.search("([0-9]+)",line))
-            missesicache.append(found[0])
-        elif "system.cpu.icache.overall_hits::total" in line:
+            numBranches.append(found[0])
+        elif "system.cpu.BranchMispred" in line:
             found = (re.search("([0-9]+)",line))
-            hitsicache.append(found[0])
-        elif "system.l2.overall_misses::total" in line:
-            found = (re.search(" ([0-9]+)",line))
-            misses2cache.append(found[0])
-        elif "system.l2.overall_hits::total" in line:
-            found = (re.search(" ([0-9]+)",line))
-            hits2cache.append(found[0])
+            BranchMispredPct.append(found[0])
+        elif "system.cpu.branchPred.condPredicted" in line:
+            found = (re.search("([0-9]+)",line))
+            condPredicted.append(found[0])
+        elif "system.cpu.branchPred.condIncorrect" in line:
+            found = (re.search("([0-9]+)",line))
+            condIncorrect.append(found[0])
+for i in range(3):
+    BTBMissPCT.append((1-(int(BTBHits[i])/int(BTBLookups[i])))*100)
+    numbranchMispred.append((int(BranchMispredPct[i])/int(numBranches[i]))*100)
 
-fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2)
-ax1.invert_yaxis()
-ax1.plot(missesdcache,'o-')
-ax1.xaxis.set_ticks(np.arange(5))
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+ax1.plot(BTBMissPCT,'o-')
+ax1.xaxis.set_ticks(np.arange(3))
 ax1.xaxis.set_ticklabels(size)
-ax1.legend(["MissesDCache"])
-ax2.plot(hitsdcache,'o-')
-ax2.legend(["HitsDCache"])
-ax2.xaxis.set_ticks(np.arange(5))
+ax1.legend(["BTBMissPCT"])
+ax2.plot(numbranchMispred,'o-')
+ax2.xaxis.set_ticks(np.arange(3))
 ax2.xaxis.set_ticklabels(size)
-ax3.invert_yaxis()
-ax3.plot(missesicache,'o-')
-ax3.legend(["MissesICache"])
-ax3.xaxis.set_ticks(np.arange(5))
+ax2.legend(["NumbMisPredBranches"])
+ax3.plot(condPredicted,'o-')
+ax3.xaxis.set_ticks(np.arange(3))
 ax3.xaxis.set_ticklabels(size)
-ax4.plot(hitsicache,'o-')
-ax4.legend(["HitsICache"])
-ax4.xaxis.set_ticks(np.arange(5))
+ax3.legend(["condPredicted"])
+ax4.plot(condIncorrect,'o-')
+ax4.xaxis.set_ticks(np.arange(3))
 ax4.xaxis.set_ticklabels(size)
-ax5.invert_yaxis()
-ax5.plot(misses2cache,'o-')
-ax5.legend(["Misses2Cache"])
-ax5.xaxis.set_ticks(np.arange(5))
-ax5.xaxis.set_ticklabels(size)
-ax6.plot(hits2cache,'o-')
-ax6.legend(["Hits2Cache"])
-ax6.xaxis.set_ticks(np.arange(5))
-ax6.xaxis.set_ticklabels(size)
+ax4.legend(["condIncorrect"])
+ax4.invert_yaxis()
 show()
